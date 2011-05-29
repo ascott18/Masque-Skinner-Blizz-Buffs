1ee6c448b0456cda3b103d7488393ff0e855edba
Andrew Scott <ascott18@gmail.com>
2011-05-29 03:27:50 +0000

    Update for Masque Cleaned up the code
    git-svn-id: https://repos.curseforge.com/wow/blizzbuffsfacade/trunk@2 68f43199-ba81-496d-a895-78ea0906a1c5

---
+++ BlizzBuffsFacade.lua
@@ -0,0 +1,80 @@
+
+local LBF = LibStub("LibButtonFacade", true)
+local LMB = LibMasque and LibMasque("Button")
+if not (LBF or LMB) then return end
+
+local f = CreateFrame("Frame")
+local addonName = ...
+local db
+
+local function SkinCallback(_, SkinID, Gloss, Backdrop, Group, _, Colors)
+	if Group then
+		db[Group].S = SkinID
+		db[Group].G = Gloss
+		db[Group].B = Backdrop
+		db[Group].C = Colors
+	end
+end
+	
+local function OnEvent(self, event, addon)
+	if addon ~= addonName then return end
+	
+	if not LMB then
+		BlizzBuffsFacade = BlizzBuffsFacade or {}
+		db = BlizzBuffsFacade
+		
+		db.Buffs = db.Buffs or {}
+		db.Debuffs = db.Debuffs or {}
+		db.TempEnchant = db.TempEnchant or {}
+		
+		local b = db.Buffs
+		local d = db.Debuffs
+		local t = db.TempEnchant
+		
+		LBF:Group("Blizzard Buffs", "Buffs"):Skin(b.S,b.G,b.B,b.C)
+		LBF:Group("Blizzard Buffs", "Debuffs"):Skin(d.S,d.G,d.B,d.C)
+		LBF:Group("Blizzard Buffs", "TempEnchant"):Skin(t.S,t.G,t.B,t.C)
+	end
+	
+	for i=1, BUFF_MAX_DISPLAY do
+		local buff = _G["BuffButton"..i]
+		if buff then
+			LBF:Group("Blizzard Buffs", "Buffs"):AddButton(buff)
+		end
+		if not buff then break end
+	end
+	
+	for i=1, BUFF_MAX_DISPLAY do
+		local debuff = _G["DebuffButton"..i]
+		if debuff then
+			LBF:Group("Blizzard Buffs", "Debuffs"):AddButton(debuff)
+		end
+		if not debuff then break end
+	end
+	
+	for i=1, NUM_TEMP_ENCHANT_FRAMES do
+		local TempEnchant = _G["TempEnchant"..i]
+		if TempEnchant then
+			LBF:Group("Blizzard Buffs", "TempEnchant"):AddButton(TempEnchant)
+		end
+	end
+		
+	LBF:RegisterSkinCallback("Blizzard Buffs", SkinCallback)
+	
+	f:SetScript("OnEvent", nil)
+end
+
+
+hooksecurefunc("CreateFrame", function (_, name) --dont need to do this for TempEnchant enchant frames because they are hard created in xml
+	if type(name) ~= "string" then return end
+	if strfind(name, "^DebuffButton") then
+		LBF:Group("Blizzard Buffs", "Debuffs"):AddButton(_G[name])
+	elseif strfind(name, "^BuffButton") then
+		LBF:Group("Blizzard Buffs", "Buffs"):AddButton(_G[name])
+	end
+end
+)
+	
+f:SetScript("OnEvent", OnEvent)
+f:RegisterEvent("ADDON_LOADED")
+
