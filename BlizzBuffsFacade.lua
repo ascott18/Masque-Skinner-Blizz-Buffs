f886c9c1c5abca12b07e1903afade33ac6a0a488
Andrew Scott <ascott18@gmail.com>
2011-11-30 01:45:05 +0000

    Renamed to "Masque Skinner: Blizz Buffs" TOC bump Fix: Temp enchant border should now be purple again instead of white.
    git-svn-id: https://repos.curseforge.com/wow/blizzbuffsfacade/trunk@12 68f43199-ba81-496d-a895-78ea0906a1c5

--- BlizzBuffsFacade.lua
+++ BlizzBuffsFacade.lua
@@ -1,45 +1,17 @@
 
-local LBF = LibStub("LibButtonFacade", true)
 local LMB = LibStub("Masque", true) or (LibMasque and LibMasque("Button"))
-local Stub = (LBF or LMB)
-if not Stub then return end
+if not LMB then return end
 
 local f = CreateFrame("Frame")
-local db
 
-local function SkinCallback(_, SkinID, Gloss, Backdrop, Group, _, Colors)
-	if Group then
-		db[Group].S = SkinID
-		db[Group].G = Gloss
-		db[Group].B = Backdrop
-		db[Group].C = Colors
-	end
+local function NULL()
 end
-	
+
 local function OnEvent(self, event, addon)
-	
-	if not LMB then
-		BlizzBuffsFacade = BlizzBuffsFacade or {}
-		LBF:RegisterSkinCallback("Blizzard Buffs", SkinCallback)
-		db = BlizzBuffsFacade
-		
-		db.Buffs = db.Buffs or {}
-		db.Debuffs = db.Debuffs or {}
-		db.TempEnchant = db.TempEnchant or {}
-		
-		local b = db.Buffs
-		local d = db.Debuffs
-		local t = db.TempEnchant
-		
-		LBF:Group("Blizzard Buffs", "Buffs"):Skin(b.S,b.G,b.B,b.C)
-		LBF:Group("Blizzard Buffs", "Debuffs"):Skin(d.S,d.G,d.B,d.C)
-		LBF:Group("Blizzard Buffs", "TempEnchant"):Skin(t.S,t.G,t.B,t.C)
-	end
-	
 	for i=1, BUFF_MAX_DISPLAY do
 		local buff = _G["BuffButton"..i]
 		if buff then
-			Stub:Group("Blizzard Buffs", "Buffs"):AddButton(buff)
+			LMB:Group("Blizzard Buffs", "Buffs"):AddButton(buff)
 		end
 		if not buff then break end
 	end
@@ -47,16 +19,18 @@ local function OnEvent(self, event, addon)
 	for i=1, BUFF_MAX_DISPLAY do
 		local debuff = _G["DebuffButton"..i]
 		if debuff then
-			Stub:Group("Blizzard Buffs", "Debuffs"):AddButton(debuff)
+			LMB:Group("Blizzard Buffs", "Debuffs"):AddButton(debuff)
 		end
 		if not debuff then break end
 	end
 	
 	for i=1, NUM_TEMP_ENCHANT_FRAMES do
 		local TempEnchant = _G["TempEnchant"..i]
+		--_G["TempEnchant"..i.."Border"].SetTexture = NULL
 		if TempEnchant then
-			Stub:Group("Blizzard Buffs", "TempEnchant"):AddButton(TempEnchant)
+			LMB:Group("Blizzard Buffs", "TempEnchant"):AddButton(TempEnchant)
 		end
+		_G["TempEnchant"..i.."Border"]:SetVertexColor(.75, 0, 1)
 	end
 	
 	f:SetScript("OnEvent", nil)
@@ -66,9 +40,9 @@ end
 hooksecurefunc("CreateFrame", function (_, name, parent) --dont need to do this for TempEnchant enchant frames because they are hard created in xml
 	if parent ~= BuffFrame or type(name) ~= "string" then return end
 	if strfind(name, "^DebuffButton%d+$") then
-		Stub:Group("Blizzard Buffs", "Debuffs"):AddButton(_G[name])
+		LMB:Group("Blizzard Buffs", "Debuffs"):AddButton(_G[name])
 	elseif strfind(name, "^BuffButton%d+$") then
-		Stub:Group("Blizzard Buffs", "Buffs"):AddButton(_G[name])
+		LMB:Group("Blizzard Buffs", "Buffs"):AddButton(_G[name])
 	end
 end
 )
